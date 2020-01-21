ce33081c8216ae7286fa9a5882715b7c7638d842
lab314-20@labeng.local <lab314-20@labeng.local>
2020-01-21 15:18:07 +0700

    send

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,1 +1,36 @@
 //status = "ONE-NIGHT-STAND";
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int age,hight,money;
+    string status;
+    cout << "Enter age : ";
+    cin >> age;
+    if (age < 20)
+    {
+        cout << "Enter hight : ";
+        cin >> hight;
+        if(hight < 160) status = "UNFRIEND";
+        else if (hight < 175) status = "FRIEND";
+        else {
+            cout << "Enter money : ";
+            cin >> money;
+            if(money > 69000000) status = "ONE-NIGHT-STAND";
+            else status = "MARRIED";
+        }
+    }
+    else{
+        if (age < 30 )
+        {
+           cout << "Enter money : ";
+            cin >> money;
+             if(money > 10000000) status = "BEST FRIEND";
+             else status = "UNFRIEND";
+        }
+        else status = "UNFRIEND";
+    }
+    cout << "Status : " << status ;
+    return 0;
+}
